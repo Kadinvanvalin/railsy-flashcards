2625a93401d99d4b6a08835614e9960f26e7e9b1
Gilad Shanan & Kadin <pair+giladshanan.kadinvanvalin@devbootcamp.com>
2017-10-09 11:57:42 -0500

    add nav links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+  helper_method :logged_in?
+
+  def authenticate!
+    redirect '/sessions/new' unless logged_in?
+  end
+
+  def authorize!(user)
+    redirect '/not_authorized' unless authorized?(user)
+  end
+
+  def authorized?(user)
+    current_user == user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :show, :create]
   resources :sessions, only: [:new, :destroy, :create]
+  get '/decks', to: 'decks#index'
+  root "decks#index"
 end
